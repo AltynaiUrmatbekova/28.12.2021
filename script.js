159126668bc878fcb1170f891d8fa545778040b7
AltynaiUrmatbekova <64131999+AltynaiUrmatbekova@users.noreply.github.com>
2021-12-28 11:41:59 +0600

    added new names

--- script.js
+++ script.js
@@ -2,13 +2,14 @@
 // play cards
 // cook dinner
 // finish math homework
-function callFriends() {
+function callFriends(friendsName) {
+    console.log(friendsName);
 console.log("Pick up the phone");
 console.log("Type in the number");
 console.log("Press call");
 console.log("Wait for the answer");
 }
-callFriends();
-callFriends();
-callFriends();
-callFriends();
+callFriends(Bakyt);
+callFriends(AkJol);
+callFriends(Seitek);
+callFriends(Nurdin);
