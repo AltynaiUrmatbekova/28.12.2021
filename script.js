44fabd7bd18355633cf3d815c5091375af0de91d
AltynaiUrmatbekova <64131999+AltynaiUrmatbekova@users.noreply.github.com>
2021-12-28 11:33:07 +0600

    Function call friends

---
+++ script.js
@@ -0,0 +1,14 @@
+// Very simple function
+// play cards
+// cook dinner
+// finish math homework
+function callFriends() {
+console.log("Pick up the phone");
+console.log("Type in the number");
+console.log("Press call");
+console.log("Wait for the answer");
+}
+callFriends();
+callFriends();
+callFriends();
+callFriends();
