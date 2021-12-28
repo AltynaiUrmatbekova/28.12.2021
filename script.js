75ba0af366f6a4303e017d67961f53fa6a56179a
AltynaiUrmatbekova <64131999+AltynaiUrmatbekova@users.noreply.github.com>
2021-12-28 12:45:24 +0600

    Added document.write

--- script.js
+++ script.js
@@ -2,14 +2,38 @@
 // play cards
 // cook dinner
 // finish math homework
-function callFriends(friendsName) {
+function callFriends(friendsName = "Seitek") {
     console.log(friendsName);
 console.log("Pick up the phone");
-console.log("Type in the number");
+console.log("Type in the number of" + friendsName);
 console.log("Press call");
 console.log("Wait for the answer");
 }
-callFriends(Bakyt);
-callFriends(AkJol);
-callFriends(Seitek);
-callFriends(Nurdin);
+
+// callFriends("Seitek");
+// callFriends("Nurdin");
+// callFriends("Bolot");
+// callFriends("Aijan");
+
+function calculateRectArea(a, b) {
+    console.log(a * b);
+}
+// calculateRectArea(5, 10);
+// calculateRectArea(10, 11);
+// calculateRectArea(1, 0);
+
+// r2h selindr
+function calculateCylinderVolume(r, h) {
+return 3.14 * r * r * h;
+}
+var smallGlassVol = calculateCylinderVolume(3, 4);
+var bottleVol = calculateCylinderVolume(3, 10);
+
+// console.log(smallGlassVol);
+// console.log(bottleVol);
+
+document.write("<h1>Kyrgyzstan</h1>");
+document.write("<p>Hello world!</p>");
+document.write("Hello world!");
+document.write("Bye world!<br>");
+document.write("Bye world!<br>");
